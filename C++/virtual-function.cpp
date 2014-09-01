0632f490f7e4784ddfdb25b1b96712900d9a279d
junqiao wang <junqiao@google.com>
2014-09-01 10:18:07 +0800

    .

---
+++ C++/virtual-function.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+class A {
+public:
+	virtual void sss() {
+		printf("a sss\n");
+	}
+	void ttt() {
+		printf("a ttt\n");
+	}
+};
+
+class B : public A {
+public:
+	virtual void sss() {
+		printf("b sss\n");
+	}
+	void ttt() {
+		printf("b ttt\n");
+	}
+};
+
+int main() {
+	A *b = new B();
+	b->sss();
+	b->ttt();
+}
